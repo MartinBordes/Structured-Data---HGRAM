2f26c73c8e07037921d4ca4c61f802c0c2597922
mbordes-ensae <martin.bordes@ensae.fr>
2024-04-05 08:12:44 +0000

    Good notebooks

--- node_process.py
+++ node_process.py
@@ -35,17 +35,30 @@ def main():
     val_df_list = []
     test_df_list = []
     for idx, G in enumerate(dgl_Gs):
-        train_ind = [index.item() for index in torch.nonzero(G.ndata['train_mask'])]
-        train = df[df.name.str.contains(str(idx)+'_'+str(train_ind))]
-        train_df_list.append(train)
-        
-        val_ind = [index.item() for index in torch.nonzero(G.ndata['val_mask'])]
-        val = df[df.name.str.contains(str(idx)+'_'+str(val_ind))]
-        val_df_list.append(val)
-        
-        test_ind = [index.item() for index in torch.nonzero(G.ndata['test_mask'])]
-        test = df[df.name.str.contains(str(idx)+'_'+str(test_ind))]
-        test_df_list.append(test)
+        if G.ndata['train_mask'].shape[1] > 1:
+            train_ind = [index.item() for index in torch.nonzero(G.ndata['train_mask'][:,0])[:,0]]
+            train = df[df.name.str.contains(str(idx)+'_'+str(train_ind))]
+            train_df_list.append(train)
+            
+            val_ind = [index.item() for index in torch.nonzero(G.ndata['val_mask'][:,0])[:,0]]
+            val = df[df.name.str.contains(str(idx)+'_'+str(val_ind))]
+            val_df_list.append(val)
+            
+            test_ind = [index.item() for index in torch.nonzero(G.ndata['test_mask'][:,0])[:,0]]
+            test = df[df.name.str.contains(str(idx)+'_'+str(test_ind))]
+            test_df_list.append(test)
+        else:
+            train_ind = [index.item() for index in torch.nonzero(G.ndata['train_mask'])]
+            train = df[df.name.str.contains(str(idx)+'_'+str(train_ind))]
+            train_df_list.append(train)
+            
+            val_ind = [index.item() for index in torch.nonzero(G.ndata['val_mask'])]
+            val = df[df.name.str.contains(str(idx)+'_'+str(val_ind))]
+            val_df_list.append(val)
+            
+            test_ind = [index.item() for index in torch.nonzero(G.ndata['test_mask'])]
+            test = df[df.name.str.contains(str(idx)+'_'+str(test_ind))]
+            test_df_list.append(test)
         
     train_df = pd.concat(train_df_list)
     val_df = pd.concat(val_df_list)
@@ -71,7 +84,7 @@ def main():
 if __name__ == '__main__':
 
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('--dataset', type=int, help='dataset Name (possibilities : CoraGraphDataset, CiteseerGraphDataset, PubmedGraphDataset... (see https://docs.dgl.ai/en/2.0.x/api/python/dgl.data.html#node-prediction-datasets)', default=CoraGraphDataset)
+    argparser.add_argument('--dataset', type=type, help='dataset Name (possibilities : CoraGraphDataset, CiteseerGraphDataset, PubmedGraphDataset... (see https://docs.dgl.ai/en/2.0.x/api/python/dgl.data.html#node-prediction-datasets)', default=CoraGraphDataset)
 
     args = argparser.parse_args()
 
